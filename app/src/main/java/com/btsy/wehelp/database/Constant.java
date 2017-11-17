2868100bf28c119696caad6b850a2d0de4d217ea
zy\zhouzhongbo <zhouzhongbo@droi.com>
2017-11-17 17:52:13 +0800

    up

---
+++ app/src/main/java/com/btsy/wehelp/database/Constant.java
@@ -0,0 +1,40 @@
+package com.btsy.wehelp.database;
+
+/**
+ * Created by zhouzhongbo on 2017/11/17.
+ */
+
+public class Constant {
+    String DBNAME="wehelper.db";
+//  public column define
+    String COLUMN_PUBLIC_DEVICEID="DEVICEID";
+    String COLUMN_PUBLIC_CREATETIME="CREATETIME";
+    String COLUMN_PUBLIC_MODEFYTIME="MODEFYTIME";
+
+//  customer tab and column define
+    String TAB_CUSTOMER="customer";
+    String COLUMN_CUSTOMER_ID="ID";
+    String COLUMN_CUSTOMER_NAME="NAME";
+    String COLUMN_CUSTOMER_PHONE="PHONE";
+    String COLUMN_CUSTOMER_ADDRESS="ADDRESS";
+    String COLUMN_CUSTOMER_SEX="SEX";
+
+//  user tab and column define
+    String TAB_USER="user";
+    String COLUMN_USER_ID="ID";
+    String COLUMN_USER_NAME="NAME";
+    String COLUMN_USER_NICKNAME="NICKNAME";
+    String COLUMN_USER_ICON="ICON";
+    String COLUMN_USER_SEX="SEX";
+    String COLUMN_USER_CHECK="CHECK";
+    String COLUMN_USER_INTEGRATION="INTEGRATION";
+    String COLUMN_USER_VIP="VIP";
+    String COLUMN_USER_ROLO="ROLO";
+
+    String TAB_PRODUCT="product";
+    String TAB_ORDER="order";
+    String TAB_STOCK="stock";
+    String TAB_VIPINFO="vip";
+    String TAB_PRODUCTINORDER="productinorder";
+
+}
---
+++ app/src/main/java/com/btsy/wehelp/database/DbHelper.java
@@ -0,0 +1,70 @@
+package com.btsy.wehelp.database;
+
+import android.content.Context;
+import android.database.DatabaseErrorHandler;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by zhouzhongbo on 2017/11/17.
+ */
+
+public class DbHelper extends SQLiteOpenHelper {
+    public DbHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    public DbHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version, DatabaseErrorHandler errorHandler) {
+        super(context, name, factory, version, errorHandler);
+    }
+
+    @Override
+    public String getDatabaseName() {
+        return super.getDatabaseName();
+    }
+
+    @Override
+    public void setWriteAheadLoggingEnabled(boolean enabled) {
+        super.setWriteAheadLoggingEnabled(enabled);
+    }
+
+    @Override
+    public SQLiteDatabase getWritableDatabase() {
+        return super.getWritableDatabase();
+    }
+
+    @Override
+    public SQLiteDatabase getReadableDatabase() {
+        return super.getReadableDatabase();
+    }
+
+    @Override
+    public synchronized void close() {
+        super.close();
+    }
+
+    @Override
+    public void onConfigure(SQLiteDatabase db) {
+        super.onConfigure(db);
+    }
+
+    @Override
+    public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        super.onDowngrade(db, oldVersion, newVersion);
+    }
+
+    @Override
+    public void onOpen(SQLiteDatabase db) {
+        super.onOpen(db);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
